559c97a453834a35aa81faf5b96f2efc422da091
RutKoren <Ruth-koren@gmail.com>
2026-02-05 13:39:19 +0200

    implement menu component with HTML, CSS, and test files; update routing and asset paths

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: MenuComponent },
+];
